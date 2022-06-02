6dd1555b6619f179f96d4224f9f2b4674d04eee2
finpangestu <195410015@students.akakom.ac.id>
2022-06-03 03:44:29 +0700

    add product-create (50%)

--- src/script/sidebar.js
+++ src/script/sidebar.js
@@ -1,3 +1,4 @@
+//sidebar
 const body = document.querySelector('body');
 const header = document.querySelector('header');
 const sidebar = body.querySelector('nav');
@@ -5,4 +6,10 @@ const toggle = header.querySelector('.fa-bars');
 
 toggle.addEventListener('click', () => {
     sidebar.classList.toggle('close');
-})
+});
+
+//uploadImage
+const loadFile = (event) => {
+    const image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
