64ede19dd639eabe5657522885ee31c0a41c6f5d
finpangestu <195410015@students.akakom.ac.id>
2022-06-02 12:08:34 +0700

    sidebar animation

---
+++ src/script/index.js
@@ -0,0 +1,8 @@
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const sidebar = body.querySelector('nav');
+const toggle = header.querySelector('.fa-bars');
+
+toggle.addEventListener('click', () => {
+    sidebar.classList.toggle('close');
+})
