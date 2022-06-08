6400f66759bfd2fec91d08e3f005a69c57113f09
finpangestu <195410015@students.akakom.ac.id>
2022-06-08 12:41:21 +0700

    add body home-page

--- src/script/index.js
+++ src/script/index.js
@@ -9,7 +9,7 @@ const showSlides = () => {
     }
     slideIndex++;
     if (slideIndex > slides.length) {
-        slideIndex = 1
+        slideIndex = 1;
     }    
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 5000);
