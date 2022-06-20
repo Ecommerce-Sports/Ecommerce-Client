06416ebc1366c4daf81299db778105b599df1f9a
cahyaseptia11 <cahyaseptia011@gmail.com>
2022-06-20 10:48:54 +0700

    add about

---
+++ src/script/about.js
@@ -0,0 +1,8 @@
+function iconMenu() {
+    const nav = document.getElementById("nav-links");
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
+}
