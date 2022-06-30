5a1fa295ec8eff4a045da25b564ffc200dd27715
finpangestu <195410015@students.akakom.ac.id>
2022-06-30 22:12:59 +0700

    add checkout-page and payment-page

--- src/script/index.js
+++ src/script/index.js
@@ -41,3 +41,17 @@ function loadFile(event) {
     const image = document.getElementById('output');
     image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+//dropdown
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    
+    window.onclick = (e) => {
+    if (!e.target.matches('.dropbtn')) {
+        const myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+}
+}
